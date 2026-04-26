27d4b087ceecd4b6ec754f72ab67538b2b9161d7
joey <joey@USF-Guest-Wifi-154-158.laptops.usf.edu>
2026-04-26 06:42:54 -0400

    Polish scanner animation and scrolling

--- phishproof-qr/demo-site/app.js
+++ phishproof-qr/demo-site/app.js
@@ -887,7 +887,9 @@ function runAnalysis({ countScan = true } = {}) {
     const result = analyzePayload(dom.urlInput.value);
     if (countScan) recordScan();
     renderAnalysis(result);
-    dom.resultsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+    window.requestAnimationFrame(() => {
+      dom.resultsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
   } catch (error) {
     window.alert(error.message);
   }
